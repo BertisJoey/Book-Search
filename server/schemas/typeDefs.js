eacd5151ecc9474d79b5c5cb2304184b040e3415
BertisJoey <joeywiesner@gmail.com>
2023-11-03 14:01:41 -0400

    added typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: string
+        saveBooks: [Book] 
+    }
+    type Book {
+        _id: ID
+        authors: String
+        description: String
+        bookId: String
+        image: String
+        link: String
+        tite: String
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(
+            userId: ID!, 
+            authors: String,
+            description: String,
+            bookId: String,
+            image: String,
+            link: String,
+            title: String!
+        ): User
+        removeBook(bookId: ID!): User
+    }
+`;
+
+module.exports = typeDefs;
